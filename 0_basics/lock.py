92f7538e96df59f2cdf65a2032533d8c621902cf
Dan Thompson <dthompsonpython@gmail.com>
2019-10-02 19:04:56 -0400

    Added in lock.py example

---
+++ 0_basics/lock.py
@@ -0,0 +1,30 @@
+import threading
+
+
+
+# We've got a global variable
+count = 0
+# Here's a lock for it
+a = threading.Lock()
+
+def worker(*args):
+    """thread worker function, with some funky ordered logic"""
+    global count
+    # We're using the lock as a context here, so it gets released
+    # at the end of this block
+    with args[0]:
+        count += 1
+        print(count)
+        count += 4
+        print(count)
+        count -= 4
+        print(count)
+
+
+# Two will execute, last one will as one finishes
+thread_1 = threading.Thread(target=worker, args=[a])
+thread_2 = threading.Thread(target=worker, args=[a])
+thread_3 = threading.Thread(target=worker, args=[a])
+thread_1.start()
+thread_2.start()
+thread_3.start()
